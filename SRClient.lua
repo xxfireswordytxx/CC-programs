bace1a902adc830f88734a05fdabb3e5c0fca3de
xxfireswordytxx <68258979+xxfireswordytxx@users.noreply.github.com>
2022-06-09 10:20:53 -0400

    Update SRClient.lua

--- SRClient.lua
+++ SRClient.lua
@@ -1,33 +1,37 @@
+local S = fs.open("SRSPortconf.txt", "r")
+local SF = S.readLine()
+S.close()
+
+local C = fs.open("SRCPortconf.txt", "r")
+local CF = C.readLine()
+C.close()
+
 local modem = peripheral.find("modem") or error("No modem attached", 0)
 local args = {...}
 local RCNI = 0
-local c = 15
-local rc = 43
 local v = tonumber(args [1])
 local ACK = tonumber(message)
 local m = string
-local P = fs.open("CID.txt", "r")
-local PC = P.readLine()
-P.close()
 
 
-modem.open(tonumber(PC))
+
+modem.open(tonumber(SF))
  
 if v == 1 then
     print("Repeat RCNI: "..tostring(RCNI))
-    print("port 43 Open: "..tostring(modem.isOpen(43)))
-    print("port 15 Open: "..tostring(modem.isOpen(15)))
+    print("port 43 Open: "..tostring(modem.isOpen(tonumber(CF))))
+    print("port 15 Open: "..tostring(modem.isOpen(tonumber(SF))))
 end
 
 term.write("RCNI: ")
 RCNI = read()
 
-modem.transmit(15, 43, tostring(RCNI))
+modem.transmit(tonumber(SF), tonumber(CF), tostring(RCNI))
 
 local event, side, channel, replyChannel, message, distance
 repeat
   event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-until channel == 43
+until channel == tonumber(CF)
 
 if v == 1 then
     print("Connection status: "..message)
