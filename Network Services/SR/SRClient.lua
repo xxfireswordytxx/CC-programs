81819067fcd3aa8ffb04cd393e09e2c9628b7bea
xxfireswordytxx <68258979+xxfireswordytxx@users.noreply.github.com>
2022-06-09 18:46:59 -0400

    Update SRClient.lua

--- Network Services/SR/SRClient.lua
+++ Network Services/SR/SRClient.lua
@@ -1,37 +1,29 @@
-local S = fs.open("SRSPortconf.txt", "r")
-local SF = S.readLine()
-S.close()
-
-local C = fs.open("SRCPortconf.txt", "r")
-local CF = C.readLine()
-C.close()
-
 local modem = peripheral.find("modem") or error("No modem attached", 0)
 local args = {...}
 local RCNI = 0
+local c = 15
+local rc = 43
 local v = tonumber(args [1])
 local ACK = tonumber(message)
 local m = string
 
-
-
-modem.open(tonumber(SF))
+modem.open(43)
  
 if v == 1 then
     print("Repeat RCNI: "..tostring(RCNI))
-    print("port 43 Open: "..tostring(modem.isOpen(tonumber(CF))))
-    print("port 15 Open: "..tostring(modem.isOpen(tonumber(SF))))
+    print("port 43 Open: "..tostring(modem.isOpen(43)))
+    print("port 15 Open: "..tostring(modem.isOpen(15)))
 end
 
 term.write("RCNI: ")
 RCNI = read()
 
-modem.transmit(tonumber(SF), tonumber(CF), tostring(RCNI))
+modem.transmit(15, 43, tostring(RCNI))
 
 local event, side, channel, replyChannel, message, distance
 repeat
   event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-until channel == tonumber(CF)
+until channel == 43
 
 if v == 1 then
     print("Connection status: "..message)
