d97928602ec08238a2ff8831c72cbb024f8afdf1
xxfireswordytxx <68258979+xxfireswordytxx@users.noreply.github.com>
2022-11-18 11:04:06 -0500

    Update turbine-reactor-control.lua

--- utilities/turbine-reactor-control.lua
+++ utilities/turbine-reactor-control.lua
@@ -1,7 +1,12 @@
+while true do 
+
+
+--local variables 
 local turbine = peripheral.wrap("left")
 local reactor = peripheral.wrap("right")
 local mon = peripheral.wrap("top")
 
+--other variables 
 tenergy = turbine.getEnergyStored()
 tenergym = turbine.getEnergyCapacity()
 tenergyp = tenergy / tenergym * 100
@@ -13,4 +18,41 @@ rctemp = reactor.getCasingTemperature()
 rctempflo = math.floor(rctemp * 100) / 100
 
 turotspe = turbine.getRotorSpeed()
+turotspeflo = math.floor(turotspe * 100) / 100
+ 
+--refresh time 
+  os.sleep(0.2)
+  
+--monitor output
+  mon.clear()
+  mon.setCursorPos(1,2)
+  mon.write("Fuel-Temp")
+  mon.setCursorPos(11,2)
+  mon.write(fueltemp)
+  mon.setCursorPos(1,3)
+  mon.write("Case-Temp")
+  mon.setCursorPos(11,3)
+  mon.write(casetemp)
+  mon.setCursorPos(1,4)
+  mon.write("Energy") 
+  mon.setCursorPos(11,4)
+  mon.write(energypercent.."%")
+  mon.setCursorPos(1,5)   
+  mon.write("Waste")
+  mon.setCursorPos(11,5)
+  mon.write(waste)
+  mon.setCursorPos(1,6)
+  mon.write("Rod-insertion")
+  mon.setCursorPos(18,6)
+  mon.write(rodinsertion.."%")
+  
+  
+  
+  
+  
+  
+
+
+
+
 
