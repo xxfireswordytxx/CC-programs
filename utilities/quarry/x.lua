b5c7bace121c8c432dc00549780d017d5c617d30
xxfireswordytxx <68258979+xxfireswordytxx@users.noreply.github.com>
2024-12-03 00:15:39 -0500

    Update x.lua

--- utilities/quarry/x.lua
+++ utilities/quarry/x.lua
@@ -3,13 +3,17 @@ modem = peripheral.find("modem")
 modem.open(1)
 
 while true do
+    local event, side, channel, replyChannel, message, distance
     repeat
         event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
     until channel == 1
 
     if message == "x+" then
         gear.move(8, -2)
+        print("3")
     elseif message == "x-" then
         gear.move(64, 2)
+        print("2")
     end
+    print("1")
 end
