b9b5801fa6aebfa4ffa995cac694f14ed2a93d23
xxfireswordytxx <68258979+xxfireswordytxx@users.noreply.github.com>
2024-12-03 00:15:57 -0500

    Update y.lua

--- utilities/quarry/y.lua
+++ utilities/quarry/y.lua
@@ -2,12 +2,13 @@ modem = peripheral.find("modem")
 modem.open(3)
 
 while true do
+    local event, side, channel, replyChannel, message, distance
     repeat
-        vent, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+        event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
     until channel == 3
     if message == "y-" then
         redstone.setOutput("back", true)
-        os.sleep(300)
+        sleep(300)
         redstone.setOutput("back", false)
     end
 end
